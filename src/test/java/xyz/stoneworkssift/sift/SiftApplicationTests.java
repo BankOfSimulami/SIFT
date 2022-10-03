bec8877d5512225453fe759cc6a1162b7fec81a9
Redstoneguy129 <public@cameronwhyte.me>
2022-10-03 16:28:23 +0100

    Base SpringBoot

---
+++ src/test/java/xyz/stoneworkssift/sift/SiftApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stoneworkssift.sift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
